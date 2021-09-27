09cdea606855591ac3c56929024017a03e6c2eef
Rick Huisman <rick@idea-x.nl>
2021-09-27 15:58:37 +0200

    0.25

--- view/full/index.php
+++ view/full/index.php
@@ -1,6 +1,7 @@
 <?php 
 	if($data['connection']){
-		require('pages/home.php');
+		echo('<h1>WELKOM</h1>');
+		echo('<a href="pages/paardenoverzicht.php" target="_blank">PAARDEN</a> <a href="pages/klantenoverzicht.php" target="_blank">KLANTEN</a>');
 	}else{
 		echo '<h1>DB CONNECTION FAILED!</h1>';
 	}
--- view/full/pages/home.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo('<h1>WELKOM</h1>');
-	echo('<a href="">PAARDEN </a>')
-?>
